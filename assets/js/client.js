551b98710f036acbccf494327247ac292213f426
benooit.chocot <benoit.chocot@gmail.com>
2020-10-06 13:26:17 +0200

    refix

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,46 +1,56 @@
-// Set your publishable key: remember to change this to your live publishable key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
-const elements = stripe.elements();
-// Custom styling can be passed to options when creating an Element.
-const style = {
-    base: {
-        // Add your base input styles here. For example:
-        fontSize: '50px',
-        color: '#32325d',
-    },
-};
+$(document).ready(function(){
 
-// Create an instance of the card Element.
-const card = elements.create('card', {style});
+    // Create a Stripe client
+    const stripe = Stripe('pk_test_A45s2laXHrCRj6Tow44dk67z');
 
-// Add an instance of the card Element into the `card-element` <div>.
-card.mount('#card-element');
-// Create a token or display an error when the form is submitted.
-const form = document.getElementById('payment-form');
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+    // Create an instance of Elements
+    const elements = stripe.elements();
 
-    const {token, error} = await stripe.createToken(card);
+    // Try to match bootstrap 4 styling
+    const style = {
+        base: {
+            'lineHeight': '1.35',
+            'fontSize': '1.11rem',
+            'color': '#495057',
+            'fontFamily': 'apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif'
+        }
+    };
 
-    if (error) {
-        // Inform the customer that there was an error.
-        const errorElement = document.getElementById('card-errors');
-        errorElement.textContent = error.message;
-    } else {
-        // Send the token to your server.
-        stripeTokenHandler(token);
-    }
+    // Card number
+    const card = elements.create('cardNumber', {
+        'placeholder': '',
+        'style': style
+    });
+    card.mount('#card-number');
+
+    // CVC
+    const cvc = elements.create('cardCvc', {
+        'placeholder': '',
+        'style': style
+    });
+    cvc.mount('#card-cvc');
+
+    // Card number
+    const exp = elements.create('cardExpiry', {
+        'placeholder': '',
+        'style': style
+    });
+    exp.mount('#card-exp');
+
+    // Submit
+    $('#payment-submit').on('click', function(e){
+        e.preventDefault();
+        const cardData = {
+            'name': $('#name').val()
+        };
+        stripe.createToken(card, cardData).then(function(result) {
+            console.log(result);
+            if(result.error && result.error.message){
+                alert(result.error.message);
+            }else{
+                alert(result.token.id);
+            }
+        });
+    });
+
 });
-const stripeTokenHandler = (token) => {
-    // Insert the token ID into the form so it gets submitted to the server
-    const form = document.getElementById('payment-form');
-    const hiddenInput = document.createElement('input');
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', 'stripeToken');
-    hiddenInput.setAttribute('value', token.id);
-    form.appendChild(hiddenInput);
-
-    // Submit the form
-    form.submit();
-}
