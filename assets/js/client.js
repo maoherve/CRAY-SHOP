a598de8e71109755c3c54e1234cdfc45eeb41f5a
benooit.chocot <benoit.chocot@gmail.com>
2020-10-06 12:20:03 +0200

    chages

--- assets/js/client.js
+++ assets/js/client.js
@@ -16,3 +16,31 @@ const card = elements.create('card', {style});
 
 // Add an instance of the card Element into the `card-element` <div>.
 card.mount('#card-element');
+// Create a token or display an error when the form is submitted.
+const form = document.getElementById('payment-form');
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const {token, error} = await stripe.createToken(card);
+
+    if (error) {
+        // Inform the customer that there was an error.
+        const errorElement = document.getElementById('card-errors');
+        errorElement.textContent = error.message;
+    } else {
+        // Send the token to your server.
+        stripeTokenHandler(token);
+    }
+});
+const stripeTokenHandler = (token) => {
+    // Insert the token ID into the form so it gets submitted to the server
+    const form = document.getElementById('payment-form');
+    const hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+
+    // Submit the form
+    form.submit();
+}
